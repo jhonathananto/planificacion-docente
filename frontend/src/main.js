d0eefa9d852f89f74a14179a21524c62d4dad02d
Jhonathan Rodriguez <jhonathan.rodriguez@instipp.edu.ec>
2025-03-02 12:37:14 -0500

    Se instala el tema Aura de PrimVue para el Frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,20 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura'
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+      preset: Aura
+    }
+})
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
